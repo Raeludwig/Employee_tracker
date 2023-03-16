78fca003e5e2f8cc3e9b5817058424fb5be979e2
Raeludwig <racheleludwig@gmail.com>
2023-03-16 00:10:03 -0600

    fixed inquirer

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,24 @@ CREATE DATABASE employee_db;
 USE employee_db;
 
 CREATE TABLE department (
-  id: INT PRIMARY KEY,
-  name: VARCHAR(30)
+  id: INT PRIMARY KEY AUTO_INCREMENT,
+  department_name: VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE reviews (
-    id: INT PRIMARY KEY,
-    name: VARCHAR(30),
-    salary: DECIMAL.
-    department_id:INT
+CREATE TABLE roles (
+    id: INT PRIMARY KEY AUTO_INCREMENT,
+    title: VARCHAR(30) UNIQUE NOT NULL,
+    salary: DECIMAL NOT NULL,
+    department_id:INT NOT NULL
+    FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id: INT PRIMARY KEY,
-    first_name: VARCHAR(30),
-    last_name: VARCHAR(30),
-    role_id: INT.
+    id: INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    first_name: VARCHAR(30) NOT NULL,
+    last_name: VARCHAR(30) NOT NULL,
+    role_id: INT NOT NULL,
+    FOREIGN KEY (roles_id) REFERENCES roles(id),
     manager_id:INT
+    FOREIGN KEY (manager_id) REFERENCES employees(id)    
 );
